7e9a133dd8959e7d53b1602faa7bb76a291e22a4
guha-mahesh <mahesh.g@northeastern.edu>
2025-04-17 18:20:11 -0400

    perms

--- ClubPuffin/connect.ts
+++ ClubPuffin/connect.ts
@@ -24,7 +24,7 @@ const SECRET_KEY = process.env.JWT_SECRET || "your_secret_key";
 
 
 app.use(cors({
-  origin: ["https://club-puffin-wyst.vercel.app"],
+  origin: ["https://club-puffin.onrender.com"],
   methods: "GET,POST",
   credentials: true,
 }));
